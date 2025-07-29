313b79185b17ccba1c0d81bfbcd22928523f3feb
Min-Nhut <nguyennhut2101@gmail.com>
2025-07-29 19:00:55 +0700

    hoan thanh backend

--- src/route/web.js
+++ src/route/web.js
@@ -13,9 +13,14 @@ let init_Web_Routes = (app) => {
     router.get('/edit-crud', home_Controller.getEditCRUD);
 
     router.post('/put-crud', home_Controller.putCRUD);
-    router.get('/delete-crud', home_Controller.deleteCRUD)
+    router.get('/delete-crud', home_Controller.deleteCRUD);
 
-    router.post('/api/login', userController.handleLogin)
+    router.post('/api/login', userController.handleLogin);
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser);
+    
 
     return app.use("/", router);
 }
